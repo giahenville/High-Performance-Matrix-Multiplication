4210de1b0748cae938ad95b059c8c71b03ead308
Gia Henville <140769584+giabui@users.noreply.github.com>
2025-08-21 23:52:36 -0400

    FileToMatrix reads from MatA.txt and MatB.txt
    done to parse externally generated matrices

--- main.cpp
+++ main.cpp
@@ -1,67 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <fstream>
+#include <sstream>
 
 using Matrix = std::vector<std::vector<int>>;
 
-Matrix generateMatrix(int size);
-long long timeToExecute(Matrix& A, Matrix& B);
-Matrix matrixMultiplication(const Matrix& A, const Matrix& B);
+Matrix generateMatrix(size_t N);
+void timeToExecute(Matrix& A, Matrix& B, std::function<Matrix(const Matrix&, const Matrix&)>);
+Matrix naiveMMUL(const Matrix& A, const Matrix& B);
 void printMatrix(const Matrix& matrix);
+Matrix FileToMatrix(std::string_view filepath);
 
 int main(){
+    /*
     srand(time(0)); // seed random once
-    int size = (rand() % 5) + 2; // random size between 2 and 6
+    //int size = (rand() % 5) + 2; // random size between 2 and 6
+    int size = 10;
     
     Matrix matrix1 = generateMatrix(size);
     Matrix matrix2 = generateMatrix(size);
-
+    
     std::cout << "Matrix1:\n";
     printMatrix(matrix1);
     std::cout << "Matrix2:\n";
     printMatrix(matrix2);
+    */
 
-    long long result = timeToExecute(matrix1, matrix2);
+    // read from external files
+    Matrix MatA = FileToMatrix("MatA.txt");
+    Matrix MatB = FileToMatrix("MatB.txt");
 
-    std::cout << "Time taken to execute: " << result << " microseconds \n";
-    // std::cout << "type of result: " << typeid(result).name() << "\n";
+    std::cout << "C++: naiveMMUL\n";
+    timeToExecute(MatA, MatB, naiveMMUL);
     return 0;
 }
 
-Matrix generateMatrix(int size){
-
-    Matrix matrix;
-
-    for (int i = 0; i < size; i++ ) {
-        std::vector<int> arr;
-        for ( int j = 0; j < size; j++ ) {
+// Generates an N by N (square) matrix
+Matrix generateMatrix(size_t N) {
+    Matrix matrix(N, std::vector<int>(N, 0));
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
             int num = rand() % 10;
-            arr.push_back(num);
+            matrix[i][j] = num;
         }
-        matrix.push_back(arr);
     }
-
     return matrix;
 };
 
 
-long long timeToExecute(Matrix& A, Matrix& B){
-
+void timeToExecute(Matrix& A, Matrix& B, std::function<Matrix(const Matrix&, const Matrix&)> mmul) {
     auto start = std::chrono::high_resolution_clock::now();
-    // TODO: run matrix multiplication algorithm here
-    
-    Matrix res = matrixMultiplication(A, B);
+   
+    // function starts running here!
+    mmul(A, B);
     
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    std::cout << "Result:\n";
-    printMatrix(res);
-    return duration.count();
-
+    std::cout << "time to execute: " << duration.count() << "\n";
 };
 
-// Function to multiply two square matrices
-Matrix matrixMultiplication(const Matrix& A, const Matrix& B) {
+// Function to multiply two square matrices using an O(N^3) loop
+Matrix naiveMMUL(const Matrix& A, const Matrix& B) {
     size_t n = A.size();
     Matrix C(n, std::vector<int>(n, 0));
 
@@ -83,3 +83,24 @@ void printMatrix(const Matrix& matrix) {
         std::cout << "\n";
     }
 };
+
+Matrix FileToMatrix(std::string_view filepath) {
+    Matrix resultant;
+
+    std::ifstream file(filepath);
+    std::string line;
+
+    while (std::getline(file, line)) {
+        std::vector<int> tokens;
+        std::string token;
+        std::stringstream ss(line);
+
+        while (std::getline(ss, token, ' ')) {
+            tokens.push_back(stoi(token));
+        }
+
+        resultant.push_back(tokens);
+    }
+
+    return resultant;
+}
